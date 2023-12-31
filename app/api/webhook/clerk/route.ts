2e40c16716560b3499adaf8560dd740c6ab9fa33
salmanharitsi <salmanalharitsi14@gmail.com>
2023-12-31 19:45:50 +0700

    update

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -59,10 +59,10 @@ export async function POST(req: Request) {
 
     const user = {
       clerkId: id,
-      email: email_addresses[0].email_address,
-      username: username!,
       firstName: first_name,
       lastName: last_name,
+      username: username!,
+      email: email_addresses[0].email_address,
       photo: image_url,
     }
 
--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,19 @@
 import { authMiddleware } from "@clerk/nextjs";
-
+ 
 export default authMiddleware({
-    publicRoutes: [
-      '/',
-      '/books/:id',
-      '/api/webhook/clerk',
-      '/api/webhook/stripe',
-      '/api/uploadthing'
-    ],
-    ignoredRoutes: [
-      '/api/webhook/clerk',
-      '/api/webhook/stripe',
-      '/api/uploadthing'
-    ]
-  });
-   
+  publicRoutes: [
+    '/',
+    '/events/:id',
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ],
+  ignoredRoutes: [
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ]
+});
  
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
