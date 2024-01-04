241f46c08e264880b99ebf5bab4b190b9fdd8d01
salmanharitsi <salmanalharitsi14@gmail.com>
2024-01-04 11:17:05 +0700

    update

--- lib/validator.ts
+++ lib/validator.ts
@@ -3,7 +3,7 @@ import * as z from "zod"
 export const bookFormSchema = z.object({
   title: z.string().min(3, 'Title must be at least 3 characters'),
   description: z.string().min(3, 'Description must be at least 3 characters').max(1000, 'Description must be less than 1000 characters'),
-  writer: z.string().min(3, 'Writer is required'),
+  writer: z.string().min(3, 'Author is required'),
   poster: z.string().min(3, 'This field is required'),
   imageUrl: z.string().min(3, 'Image is required'),
   categoryId: z.string(),
